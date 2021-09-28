94023f5c569a9d7b27b7b38ced929318004ff9a9
kydos <angelo@icorsaro.net>
2021-09-28 15:24:26 +0200

    added complex number example

--- labs/src/main/scala/fr/esiee/fpic/labs/Wecome.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/Wecome.scala
@@ -1,7 +1,7 @@
 package fr.esiee.fpic.labs
 
 @main
-def welcome(args: String*) =
+def welcome(args: String*): Unit =
   val name = if args.length > 1 then args(1) else "Goofy"
   println(s"$name, welcome to Functional Programmign in Scala!")
 
