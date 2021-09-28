53eca0117f50ad5b982a805b2e25b0be34a51df2
kydos <angelo@icorsaro.net>
2021-09-28 15:28:03 +0200

    updated complex type

--- labs/src/main/scala/fr/esiee/fpic/labs/Complex.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/Complex.scala
@@ -1,11 +1,6 @@
 package fr.esiee.fpic.labs:
-  import math.Numeric
-  import math.Fractional.Implicits.infixFractionalOps
-  import math.Numeric.Implicits.infixNumericOps
-  import math.Integral.Implicits.infixIntegralOps
 
   class Complex(val im: Float, val re: Float):
-
     def +(other: Complex) = Complex(re + other.re, im + other.im)
 
     def -(other: Complex) = Complex(re - other.re, im - other.im)
