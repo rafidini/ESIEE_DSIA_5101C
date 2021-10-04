816d3d246ed8b693d059387d788e99e6d17818ca
rafidini <itokiana.rafidinarivo@edu.esiee.fr>
2021-10-04 14:44:27 +0200

    Merge local

--- labs/src/main/scala/fr/esiee/fpic/labs/Complex.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/Complex.scala
@@ -1,15 +1,21 @@
 package fr.esiee.fpic.labs:
 
-  class Complex(val im: Float, val re: Float):
-    def +(other: Complex) = Complex(re + other.re, im + other.im)
+  class Complex(val re: Float, val im: Float):
+    def +(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
-    def -(other: Complex) = Complex(re - other.re, im - other.im)
+    def -(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     override def toString: String = s"$re+i$im"
-    // Please define:
-    // - modulo
-    // - multiplication
-    // - division
-    // - negation (hint: this is a unary op)
 
+    def *(other: Complex): Complex = Complex(re * other.re - im * other.im, re * other.im + other.re * im)
+
+    def /(other: Complex): Complex =
+      val den: Float = other.re * other.re + other.im * other.im
+      val re_num: Float = re * other.re + im * other.im
+      val im_num: Float = im * other.re - re * other.im
+      Complex(re_num/den, im_num/den)
+
+    def !(): Complex = Complex(-re, -im)
+
+    def %(other: Complex): Double = scala.math.sqrt(scala.math.pow(re, 2) + scala.math.pow(im, 2))
 
--- labs/src/main/scala/fr/esiee/fpic/labs/Rational.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/Rational.scala
@@ -15,6 +15,6 @@ package fr.esiee.fpic.labs:
       val d = gcd(n, m)
       Rational(n/d, m/d)
 
-    override def toString(r: Rational): String = "s($num/$den)"
+    override def toString(): String = "s($num/$den)"
 
     // Please define sum, subtraction, division and negation
