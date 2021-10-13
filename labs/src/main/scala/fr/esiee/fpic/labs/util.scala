5d4531ac5deadc6e6868bd0f78236376eeeb7e06
kydos <angelo@icorsaro.net>
2021-10-13 13:48:39 +0200

    more examples

--- labs/src/main/scala/fr/esiee/fpic/labs/util.scala
+++ labs/src/main/scala/fr/esiee/fpic/labs/util.scala
@@ -7,17 +7,31 @@ package fr.esiee.fpic.labs:
       val _ = body
       loop(n-1)(body)
 
-  def sum(xs: List[Int]): Int = xs match
-    case y::ys => y+sum(ys)
+  def lsum(xs: List[Int]): Int = xs match
+    case y::ys => y+lsum(ys)
     // Nil is the same as List(), but more idiomatic
     case Nil => 0
 
 
-  def mul(xs: List[Int]): Int = xs match
-    case y::ys => y*mul(ys)
+  def lmul(xs: List[Int]): Int = xs match
+    case y::ys => y*lmul(ys)
     case Nil => 1
 
 
   def reverse(xs: List[Int]): List[Int] = xs match
     case Nil => Nil
     case y::ys => reverse(ys) ::: List(y)
+
+  
+  def reversel(ys: List[Int]) = 
+    ys.foldLeft(List[Int]())((xs: List[Int], x: Int) => x :: xs)
+
+  def reverser(zs: List[Int]) = 
+    zs.foldRight(List[Int]())((x: Int, xs: List[Int]) => xs ::: List(x) )
+
+  def reverse_l_tr(xs: List[Int]) = 
+    @tailrec
+    def arev(rev: List[Int], rest: List[Int]): List[Int] = rest match
+      case Nil => rev
+      case x::xs => arev(x::rev, xs)
+    arev(Nil, xs)
